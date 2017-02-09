170553739e4158af6f5f0370bb47b058d65c5984
unknown <>
2017-02-09 16:13:48 +0800

    change analyze

--- analyze_sina.py
+++ analyze_sina.py
@@ -7,30 +7,41 @@ import json
 
 
 domain_name = '.weibo.com'
-cookies = chromecookies.get_chrome_cookie(domain_name)
 
-def get_json(url):
-    response = requests.get(url, cookies=cookies)  
-    html_doc = response.text.encode('gbk','ignore').decode('gbk')
-    #加载成dict
-    json_data = json.loads(html_doc,encoding='gbk')
-    """
-    转成str，保存
-    可以直接return dict，无需另存为，可以选择删除下面代码
-    暂时保留
-    """
-    file_save = open("./page.json","w+",encoding='utf-8')
-    fp = json.dumps(json_data)
-    file_save.write(fp)
-    file_save.close()
-    # return(json_data)
 
-def analyze_json(json_1):
-    json_file = open(json_1,"r",encoding='utf-8').read()
-    # json_dic = eval(json_file)
-    fp = open(json_1,'r',encoding='utf-8')
-    json_data = json.loads(fp.read())
-    return(json_data["data"])
+
+        
+
+class analyze_json():
+    def __init__(self,url,domain_name):
+        cookies = chromecookies.get_chrome_cookie(domain_name)
+        response = requests.get(url, cookies=cookies)  
+        html_doc = response.text.encode('gbk','ignore').decode('gbk')
+        self.get_json_content = json.loads(html_doc,encoding='gbk')
+
+    def save_json_file(self):
+        
+        """
+        转成str，保存(可以保留，可以删除)
+        可以直接return dict，无需另存为文件
+        也可以选择删除下面代码。暂时保留
+        """
+        file_save = open("page.json","w+",encoding='utf-8')
+        fp = json.dumps(self.get_jsons_content)
+        file_save.write(fp)
+        file_save.close()
+
+    def read_json_file(self):
+        """
+        可以选择从保存的文件中读取json
+        如果保留此功能，需要完善调用时的参数
+        """
+        json_file = open("page.json","r",encoding='utf-8').read()
+        fp = json.loads(fp.read())
+        return(fp)
+
+    def get_json_data(self):
+        album_conut = self.get_json_content["data"]["total"]
 
 if __name__ == '__main__':
     # url = 'http://photo.weibo.com/albums/get_all?uid=1588834507&page=1&count=20'
