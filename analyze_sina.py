74c3e90240e79e9701f613d579320ad01499ae2e
unknown <>
2017-02-15 11:19:01 +0800

    add photo anaylze

--- analyze_sina.py
+++ analyze_sina.py
@@ -3,41 +3,44 @@ import chromecookies
 import json
 import os
 
-class get_json_file(self):
-    def save_json_file(self,filename = '',json_data):
+class get_json_file():
+    def save_json_file(filename,json_data):
         """
         保存json文件
         """
         file_save = open(filename,"w+",encoding='utf-8')
-        fp = json.dumps(json_data)
-        file_save.write(fp)
+        # fp = json.dumps(json_data)
+        file_save.write(json_data)
         file_save.close()
 
-    def read_json_file(self,json_file_path):
+    def read_json_file(json_file_name):
         """
         可以选择从保存的文件中读取json
         如果保留此功能，需要完善调用时的参数
         """
-        json_read = open(json_file_path,"r",encoding='utf-8')
+        json_read = open(json_file_name,"r",encoding='utf-8')
         json_content = json.loads(json_read.read())
         json_read.close()
         return(json_content)
 
 class analyze_web():
-    def __init__(self, json_file_path = '', save_file = True,**key_url):
+    def __init__(self, json_file_name = '', save_file = True,**key_url):
         """
-        json_file_path 添加json的路径，如果存在会调用本地的json文，如果不添加文件路径，则不会主动读取文件
+        json_file_name 添加json的路径，如果存在会调用本地的json文，如果不添加文件路径，则不会主动读取文件
         save_file 选择是否保存json文件，默认选择保存
         """
-        if os.path.isfile(json_file_path):
-            self.get_json_content = get_json_file.read_json_file(json_file_path)
+        if os.path.isfile(json_file_name):
+            self.get_json_content = get_json_file.read_json_file(json_file_name)
         else:
+            domain_name = key_url["domain_name"]
+            url = key_url["url"]
             cookies = chromecookies.get_chrome_cookie(domain_name)
             response = requests.get(url, cookies=cookies)  
             html_doc = response.text.encode('gbk','ignore').decode('gbk')
-            self.get_json_content = json.loads(html_doc,encoding='gbk')4
+            self.get_json_content = html_doc
+            # self.get_json_content = json.loads(html_doc,encoding='gbk')
             if save_file:
-                save_json = get_json_file.save_json_file(filename = json_file_path)
+                save_json = get_json_file.save_json_file(json_file_name,html_doc)
 
 
     def get_ablum_data(self):
@@ -63,11 +66,12 @@ class analyze_web():
 
     
 if __name__ == '__main__':
-    # photo_json = http://photo.weibo.com/photos/get_all?uid=1588834507&album_id=3575479750811822&count=30&page=1&type=3
+    photo_json = "http://photo.weibo.com/photos/get_all?uid=1588834507&album_id=3575479750811822&count=30&page=1&type=3"
     # album_url = 'http://photo.weibo.com/albums/get_all?uid=1588834507&page=1&count=20'
     # domain_name = '.weibo.com'
-    album_json = 'page.json'
-    a = analyze_web(json_file_path = album_json)
-    count_album = a.get_ablum_data()
-    print(count_album)
+    # album_json = 'page.json'
+    # url = {"domain_name":".weibo.com","url":photo_json}
+    a = analyze_web(json_file_name = 'photo.json',domain_name = ".weibo.com",url = photo_json)
+    # count_album = a.get_ablum_data()
+    # print(count_album)
     
--- test1.py
+++ test1.py
@@ -1,4 +1,11 @@
-import os
-json_file_path = ''
-a = os.path.isfile(json_file_path)
-print(a)
+import requests
+import chromecookies
+import analyze_sina
+
+domain_name = ".weibo.com"
+url = "http://photo.weibo.com/photos/get_all?uid=1588834507&album_id=3575479750811822&count=30&page=1&type=3"
+cookie = chromecookies.get_chrome_cookie(domain_name)
+response = requests.get(url, cookies = cookie)  
+html_doc = response.text.encode('gbk','ignore').decode('gbk')
+print(html_doc)
+save_json = analyze_sina.get_json_file.save_json_file("photo.json",html_doc)
