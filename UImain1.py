e79434695728e1a10123d65160c65e25b9f42e5a
gsxhnd <gsxhnd@gmail.com>
2018-06-12 00:21:19 +0800

    1

--- UImain1.py
+++ UImain1.py
@@ -1,11 +1,20 @@
 import sys
 import UI.testUI
-from PyQt5.QtWidgets import QApplication, QMainWindow
+import UI.settingDialog
+from PyQt5.QtWidgets import QApplication, QMainWindow, QDialog, QWidget
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
+
+    uiMain = UI.testUI.Ui_MainWindow()
     mWindow = QMainWindow()
-    ui = UI.testUI.Ui_MainWindow()
-    ui.setupUi(mWindow)
+    uiMain.setupUi(mWindow)
+
+    uiSetting = UI.settingDialog.Ui_settingDialog()
+    settingDialog = QDialog()
+    uiSetting.setupUi(settingDialog)
+
+    settingAction = uiMain.settingAction.triggered['bool'].connect(settingDialog.show)
+
     mWindow.show()
-    sys.exit(app.exec())
+    sys.exit(app.exec_())
