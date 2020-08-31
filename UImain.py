30f2c7b6b3d2e99a303c2654bd6ba1a62605fecd
gsxhnd <gsxhnd@gmail.com>
2020-08-31 11:04:36 +0800

    delete ui code

--- UImain.py
+++
@@ -1,9 +0,0 @@
-import sys
-from PyQt5.QtWidgets import QApplication
-import PyQt5.uic
-
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    mWindow = PyQt5.uic.loadUi('./UI/testUI.ui')
-    mWindow.show()
-    sys.exit(app.exec())
